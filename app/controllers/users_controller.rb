ce3098311cad064924ead4f2c0dca8bf47eba8ef
谢凌蔚 <lewis@Lewis.local>
2018-01-18 16:22:47 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
+    redirect_to root_path if signed_in?
     @user = User.new
   end
 
   def create
+    redirect_to root_path if signed_in?
     @user = User.new(user_params)
     if @user.save
       sign_in @user
@@ -50,13 +53,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
-  end
-
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
   end
 
   def correct_user
